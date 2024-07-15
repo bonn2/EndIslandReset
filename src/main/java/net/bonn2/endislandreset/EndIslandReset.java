446026f923cacf0de01c60935626ce32b145e3e4
Brett Bonifas <bonifas.fetzer@gmail.com>
2024-07-15 15:18:04 -0400

    Initial Commit

---
+++ src/main/java/net/bonn2/endislandreset/EndIslandReset.java
@@ -0,0 +1,17 @@
+package net.bonn2.endislandreset;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EndIslandReset extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
